b27b40517927e7016cdb7721cf1427bad5f851ea
TechGeekNeXt <techgeeknext@gmail.com>
2022-01-07 20:06:15 +0530

    tg001

---
+++ src/main/java/com/techgeeknext/SpringBooJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooJpaMysqlApplication.class, args);
+	}
+
+}
