b3ffe24618c20fa429623b6ba64c9c0eb2f35ee9
tgn001 <93204909+tgn001@users.noreply.github.com>
2022-07-08 17:40:11 +0530

    Update EmployeeController.java

--- src/main/java/com/techgeeknext/controller/EmployeeController.java
+++ src/main/java/com/techgeeknext/controller/EmployeeController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-//@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:3000") //open for specific port
+//@CrossOrigin() // open for all ports
 @RestController
 public class EmployeeController {
 
